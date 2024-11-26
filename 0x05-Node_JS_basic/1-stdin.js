d9c22f5cf16998aea4c1b35205e6b45161b8a093
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-11-26 08:28:38 -0800

    udpate

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
   process.exit();
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
 });
