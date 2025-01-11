2f50c0594d3cb20fb7110456913af581efa838fe
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-11 09:10:19 +0000

    final-user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: String(result.reason) };
+    })
+  );
+}
