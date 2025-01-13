dff2ee74d1ace55bd858124a94739ebbffe2c879
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-13 17:19:03 +0000

    udpate

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,21 @@
-mport Building from './5-building';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-	  constructor(sqft, floors) {
-		      super(sqft);
-		      this._floors = floors;
-		    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-	  get floors() {
-		      return this._floors;
-		    }
+  get floors() {
+    return this._floors;
+  }
 
-	  set floors(newFloors) {
-		      this._floors = newFloors;
-		    }
+  set floors(value) {
+    this._floors = value;
+  }
 
-	  evacuationWarningMessage() {
-		      return `Evacuate slowly the ${this._floors} floors`;
-		    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
+
