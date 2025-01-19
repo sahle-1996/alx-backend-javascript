bda126ee863e8d00eb4d9b69f72c367adac417fa
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-19 20:05:24 +0000

    update

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Generates a collection of student data.
+ * @returns {Array<{id: number, firstName: string, location: string}>}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
