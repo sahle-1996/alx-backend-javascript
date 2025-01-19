218976b0ffc47c0e58425756f5087523fb58e21e
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-19 20:21:09 +0000

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,22 @@
 /**
- * Updates student grades based on their city.
+ * Updates the grades of students in a specific city.
  * @param {Array<{id: number, firstName: string, location: string}>} students - List of students.
- * @param {string} city - Target city.
+ * @param {string} city - The target city.
  * @param {Array<{studentId: number, grade: number}>} newGrades - Array of grade objects.
- * @returns {Array<{id: number, firstName: string, location: string, grade: string | number}>} Updated list of students.
+ * @returns {Array<{id: number, firstName: string, location: string, grade: string | number}>}
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const noGrade = { grade: 'N/A' };
+  const defaultGrade = { grade: 'N/A' };
 
   if (Array.isArray(students)) {
     return students
       .filter((student) => student.location === city)
       .map((student) => {
-        const gradeObj = newGrades.find((grade) => grade.studentId === student.id) || noGrade;
-        return { ...student, grade: gradeObj.grade };
+        const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
+        return {
+          ...student,
+          grade: matchedGrade ? matchedGrade.grade : defaultGrade.grade,
+        };
       });
   }
   return [];
