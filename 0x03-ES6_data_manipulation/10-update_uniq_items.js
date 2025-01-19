9e03624923b279ec4da1b88647fc7126cf0c1bb2
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-19 20:54:19 +0000

    update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 /**
- * Updates the quantity of items in the map where the quantity is 1.
- * @param {Map<string, number>} map - A map of grocery items and their quantities.
+ * Updates the quantity of items with a quantity of 1 to 100 in the map.
+ * @param {Map<String, number>} map - A map containing grocery items and their quantities.
  */
 export default function updateUniqueItems(map) {
-  if (Object.prototype.toString.call(map) !== '[object Map]') {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  
-  for (const [key, value] of map.entries()) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
