5cf763904a677902cb83dc0b9568c03e7de1777a
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-31 13:04:16 +0000

    udpate

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,12 +1,15 @@
-export default function getSanFranciscoDescription() {
-  const year = 2017;
-  const budget = {
-    income: '$119,868',
-    gdp: '$154.2 billion',
-    capita: '$178,479',
-  };
+export function taskInitial() {
+  const taskMessage = 'I prefer const when I can.';
+  return taskMessage;
+}
 
-  return `As of ${year}, it was the seventh-highest income county in the United States\
-, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
-proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+export function getSuffix() {
+  return ' is okay';
+}
+
+export function taskFollowing() {
+  let combinedMessage = 'But sometimes let';
+  combinedMessage += getSuffix();
+
+  return combinedMessage;
 }
